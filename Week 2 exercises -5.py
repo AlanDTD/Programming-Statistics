5d0956e64c37e8b0c60c5b0049fdfcff09916d2b
AlanDTD <alan.dm.nguyen@gmail.com>
2021-08-03 10:31:08 +1000

    Added start game loop with another while.

--- Week 2 exercises -5.py
+++ Week 2 exercises -5.py
@@ -5,40 +5,46 @@ import random as rd #random module
 
 print("Hi There welcome to a game of 21 ")
 
+while True: #Start game loop
+    
+    #game code    
 
-player_value = 0
+    player_value = 0
+    new_card = 0
+    
+    player_state = input("To press enter to draw a card to start \n").upper()
 
-new_card = 0
-
-player_state = input("To press enter to draw a card to start \n").upper()
-
-while player_state != 'S' and player_value <= 21:
+    while player_state != 'S' and player_value < 21:
     
    
-    #Prompt	the	player to draw one card and display the value of the card drawn.	
-    new_card = rd.randint(2,11)
-    if player_value + new_card > 21 and new_card == 11:
-        player_value += 10
-    player_value = player_value + new_card
-    
-    print("You have drawn a %d," %new_card)
-    
-    #Ask whether they want to draw another card, or finish their game.
-    print("You currently have a total of %d! \n" %player_value)
-    if player_value <= 21: #This to hide the input if the value has exceed 21 as we don't require the input
-        player_state = input("To press enter to draw a card or enter S to stay \n").upper()
-
-    
-
-else: 
-    if player_value == 21:
-        print("You Win!")
-    elif player_value > 21:
-        print("You Bust! better luck next time")
-    else:
+        #Prompt	the	player to draw one card and display the value of the card drawn.	
         new_card = rd.randint(2,11)
-        if player_value + new_card > 21:
-            print("good timing as you would have busted! with", player_value + new_card)
-        else: 
-            print("You could have kept going as you were under 21 with", player_value + new_card)
+        if player_value + new_card > 21 and new_card == 11: #Ace Value Card
+            new_card = 1
+        player_value = player_value + new_card
         
+        print("You have drawn a %d," %new_card)
+    
+        #Ask whether they want to draw another card, or stay
+        print("You currently have a total of %d! \n" %player_value)
+        if player_value < 21: #This to hide the input if the value has exceed 21 as we don't require input
+            player_state = input("To press enter to draw a card or enter S to stay \n").upper()
+            
+            
+    else:  #Win conditions 21 or bust
+        if player_value == 21:
+            print("You Win!")
+        elif player_value > 21:
+                print("You Bust! better luck next time")
+        else: #Next card drawn if player decided to stay
+            new_card = rd.randint(2,11)    
+            if player_value + new_card > 21 and new_card == 11: #changes ace value depending if greater or less than 21
+                new_card = 1
+            elif player_value + new_card > 21:
+                print("good timing as you would have busted! with", player_value + new_card) 
+            elif player_value == 21:
+                print("Too bad you could have gotten 21!")
+            else: 
+                print("You could have kept going as you were under 21 with", player_value + new_card)
+        if input("\nWould you like to play again?\n>>> ").upper() == "N": #End game loop
+            break
